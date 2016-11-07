4badb00b29c4c804fa4bd1a302b24e47cbede361
Mikey <Mikey>
2016-11-07 22:18:02 +0100

    dodano sassa działającego i git ignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,32 +2,47 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-	sass: {
-	      options: {
-	        sourceMap: true
-	      },
-	      dist: {
-	        files: {
-	          'style.css': 'cuda.sass'
-	        }
-	      }
-	    },
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'style.css': 'cuda.sass'
+        }
+      }
+    },
 
-	watch: {
-    scripts: {
-        files: ['**/*.sass'],
+    watch: {
+      scripts: {
+        files: ['cuda.sass'],
         tasks: ['sass'],
         options: {
             spawn: false
         },
-    } 
-  }
+      } 
+    },
 	
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [
+            '*.css',
+            '*.html'
+          ]
+        },
+        options: {
+          watchTask: true, 
+          server: './'
+        }
+      }
+    }
 
   });
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['sass', 'watch']);
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
 
 };
